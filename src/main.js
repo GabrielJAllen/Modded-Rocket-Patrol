8d12806dfa9c4e728459ae287cc1769483a90078
GabrielJAllen <gjallen@ucsc.edu>
2024-01-31 23:56:19 -0800

    final fr

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //Gabriel Allen
 //Rockets and Space Amoebas
 // 4 hours to completion
-// Mods chosen: Rocket Destruction Particle Emmitter, New Spaceship Type (Space Amoeba), Mouse Controls (though any mouse button works not just left click), and new random sound effects
+// Mods chosen: Rocket Destruction Particle Emmitter(5 pts), New Spaceship Type (Space Amoeba)(5pts), Mouse Controls (though any mouse button works not just left click)(5 pts), and new random sound effects(3pts)
 // Just used Phaser documentation and on site examples as well as JFXR to generate sounds
 let config = {
     type: Phaser.AUTO,
